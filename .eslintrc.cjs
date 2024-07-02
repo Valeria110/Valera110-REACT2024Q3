792a923c49bfcaf15643ed7deb2394dfbcc6fefb
Valeriya Ivashkevich <ivashkevich03@gmail.com>
2024-07-02 19:32:40 +0300

    chore: fix module resolution when importing

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,6 +10,16 @@ module.exports = {
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  settings: {
+    'import/extensions': ['.ts', 'tsx'],
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', 'tsx'],
+    },
+  },
   plugins: ['react-refresh', 'react-compiler', 'import'],
   rules: {
     'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
